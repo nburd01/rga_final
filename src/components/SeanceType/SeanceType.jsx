405bc5b9cb631c161a34b7616eb59ef790f3df79
nburd01 <n.burdon@protonmail.com>
2022-09-21 12:31:32 +0200

    Compenentize part 4

--- src/components/SeanceType/SeanceType.jsx
+++ src/components/SeanceType/SeanceType.jsx
@@ -4,23 +4,27 @@ export const SeanceType = () => {
   return (
     <div>
 
-<h6 className="_BgTitle Left">Séances</h6>
-        <h3 className="_BgSubTitle Left">Séance type</h3>
-        <p>LUNDI / MARDI :
-Accueil à partir de 17h15 au terrain
-Mise en tenue dans les vestiaires
-Début de séance à 17h45
-Fin de séance à 19h15
-DIMANCHE :
-En fonction du nombre de participants, séance pour tous à 11h sinon :
-Groupe 2012/2013 : accueil à partir de 9h
-Mise en tenue dans les vestiaires
-Début de séance 9h30 groupe
-Fin de séance 10h45
-Groupe 2010/2011 : accueil à partir de 10h30
-Mise en tenue dans les vestiaires
-Début de séance 11h
-Fin de séance 12h15</p>
+      <h6 className="_BgTitle Left">Séances</h6>
+      <h3 className="_BgSubTitle Left">Séance type</h3>
+      <h3>LUNDI / MARDI :</h3>
+      <ul>
+        <li>Accueil à partir de 17h15 au terrain</li>
+        <li>Mise en tenue dans les vestiaires</li>
+        <li>Début de séance à 17h45</li>
+        <li>Fin de séance à 19h15</li>
+      </ul>
+      <h3>DIMANCHE :</h3>
+      <ul>
+        <li>En fonction du nombre de participants, séance pour tous à 11h sinon :</li>
+        <li>Groupe 2012/2013 : accueil à partir de 9h</li>
+        <li>Mise en tenue dans les vestiaires</li>
+        <li>Début de séance 9h30 groupe</li>
+        <li>Fin de séance 10h45</li>
+        <li>Groupe 2010/2011 : accueil à partir de 10h30</li>
+        <li>Mise en tenue dans les vestiaires</li>
+        <li>Début de séance 11h</li>
+        <li>Fin de séance 12h15</li>
+      </ul>
     </div>
   )
 }
