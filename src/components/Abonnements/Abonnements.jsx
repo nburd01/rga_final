e1cb6329adfda7e148b40fd65f23fce871c5e9be
nburd01 <n.burdon@protonmail.com>
2022-09-23 10:30:10 +0200

    Container Right

--- src/components/Abonnements/Abonnements.jsx
+++ src/components/Abonnements/Abonnements.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Abonnements = () => {
   return (
 
-    <div className="container">
+    <div className="container Right">
         <h6 className="_BgTitle Right">Abonnements</h6>
         <h3 className="_BgSubTitle Right">Abonnements</h3>
     </div>
--- src/components/Renseignements/Renseignements.jsx
+++ src/components/Renseignements/Renseignements.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Renseignements = () => {
   return (
-    <div className="container">
+    <div className="container Right">
         <h6 className="_BgTitle Right">Renseignements</h6>
         <h3 className="_BgSubTitle Right">Renseignements</h3>
     </div>
