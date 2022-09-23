7f5c5ec6aba01cdd1d5461779b04199194326c2e
nburd01 <n.burdon@protonmail.com>
2022-09-23 13:04:50 +0200

    ul horaires

--- src/components/Horaires/Dim.jsx
+++ src/components/Horaires/Dim.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 
 export const Dim = () => {
   return (
-    <div className="Right">
+    <div className="container Right horaires">
     <h3>DIMANCHE</h3>
     <ul>
       <li>En fonction du nombre de participants, séance pour tous à 11h sinon :</li>
       <h4>Groupe 2012/2013 :</h4>
-      <ul className='horaires'>
+      <ul>
           <span>9h</span>
           <li>Accueil au terrain et mise en tenue dans les vestiaires</li>
         </ul>
-        <ul className='horaires'>
+        <ul>
           <span>9h30</span>
           <li>Début de séance</li>
         </ul>
-        <ul className='horaires'>
+        <ul>
           <span>10h45</span>
           <li>Fin de séance</li>
         </ul>
       <h4>Groupe 2010/2011 :</h4>
-      <ul className='horaires'>
+      <ul>
           <span>10h30</span>
           <li>Accueil au terrain et mise en tenue dans les vestiaires</li>
         </ul>
-        <ul className='horaires'>
+        <ul>
           <span>11h</span>
           <li>Début de séance</li>
         </ul>
-        <ul className='horaires'>
+        <ul>
           <span>12h15</span>
           <li>Fin de séance</li>
         </ul>
--- src/components/Horaires/LunMar.jsx
+++ src/components/Horaires/LunMar.jsx
@@ -4,7 +4,7 @@ export const LunMar = () => {
   return (
     <div className="Left">
     <h3>LUNDI / MARDI</h3>
-      <ul className='horaires'>
+      <ul className='container Left horaires'>
           <ul>
             <span>17h15</span>
             <li>Accueil au terrain et mise en tenue dans les vestiaires</li>
