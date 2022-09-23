4a05ca335b89efcb500d1c16d34aa5b6867ed767
nburd01 <n.burdon@protonmail.com>
2022-09-23 10:49:57 +0200

    Dossier inscription pdf

---
+++ src/components/Download/pdf.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+  
+const PDF = () => {
+    // Function will execute on click of button
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file
+        fetch('Dossier_inscription__RGA 22-23.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Dossier_inscription__RGA 22-23.pdf';
+                alink.click();
+            })
+        })
+    }
+    return (
+        <>
+            <center>
+                <h1>Le dossier d'inscription à nous renvoyer</h1>
+                <h3>Téléchargez les documents en cliquant sur le bouton ci-dessous</h3>
+                <button onClick={onButtonClick}>
+                    Télécharger le dossier
+                </button>
+            </center>
+        </>
+    );
+};
+  
+export default PDF;
