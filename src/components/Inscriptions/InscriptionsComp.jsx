d768e9e52d16da547919c471dc4738db8b73365f
nburd01 <n.burdon@protonmail.com>
2022-09-27 11:46:22 +0200

    pricing presentation

--- src/components/Inscriptions/InscriptionsComp.jsx
+++ src/components/Inscriptions/InscriptionsComp.jsx
@@ -10,9 +10,9 @@ export const InscriptionsComp = () => {
       <div className='Card __primary'>
         <div className='_Title'>
         <h4>Premier abonnement</h4>
-          <h5>70 euros</h5>     
+          <h5>70 €</h5>     
         </div>
-          <p>+ 5 euros par séance.</p>
+          <p>+ 5€ par séance.</p>
           <table className='abonnements'>
             <tbody>
               <tr>
@@ -45,9 +45,9 @@ export const InscriptionsComp = () => {
       <div className='Card __primary'>
         <div className='_Title'>
         <h4>Ré-abonnement</h4>
-          <h5>50 euros</h5>     
+          <h5>50 €</h5>     
         </div>
-          <p>+ 5 euros par séance.</p>
+          <p>+ 5€ par séance.</p>
           <table className='abonnements'>
             <tbody>
               <tr>
@@ -73,8 +73,8 @@ export const InscriptionsComp = () => {
         <div className='Card __secondary '>
         <h4>Exemple</h4>
             <ul>
-              <li>Cycle 1 = 1er Abonnement + 2 spécifiques : 70 + 10 = 70 euros</li>
-              <li>Cycle 2 = 2 spécifiques : 10 euros</li>
+              <li>Cycle 1 = 1er Abonnement + 2 spécifiques : 70 + 10 = 70 €</li>
+              <li>Cycle 2 = 2 spécifiques : 10 €</li>
             </ul>
         </div>
       </div> */}
