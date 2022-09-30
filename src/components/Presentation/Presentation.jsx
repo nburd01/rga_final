61e361b0b53bcca382a45dea04d7e91954822ef8
nburd01 <n.burdon@protonmail.com>
2022-09-30 13:51:00 +0200

    typo

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -14,8 +14,8 @@ export const Presentation = () => {
               <h4>À propos de la RGA</h4>
             <li>La Roazhon Goal Academy est la <span>première structure spécifique</span> dédiée
     aux gardiennes et gardiens de but d'Ille et Vilaine.</li>
-            <li>Créée en septembre 2019 par Christophe REVEL, <span>elle répond à une problématique d'entrainement</span>
-     rencontrée dans les clubs du district et par les goals du 35.</li>
+            <li>Créée en septembre 2019 par Christophe REVEL, elle <span>répond à une problématique d'entrainement</span>
+      rencontrée dans les clubs du district et par les goals du 35.</li>
           <li>La Roazhon Goal Academy est <span>au service des passionnés</span> du poste et
     des clubs qui lui font confiance... L’année dernière 22 gardiens de but de 14
     clubs différents !</li>
