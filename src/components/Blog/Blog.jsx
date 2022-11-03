7c2ab3db20931c42506fa018672a78c2d7f1e61e
nburd01 <n.burdon@protonmail.com>
2022-11-03 15:26:19 +0100

    blog elements with img + style

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,6 +2,7 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { db } from "../../firebase";
 import { Link } from "react-router-dom";
+import './Blog.scss'
 
 export default function Blogs() {
   const [articles, setArticles] = useState([]);
@@ -21,10 +22,17 @@ export default function Blogs() {
 
 
   return (
-    <div>
+    <div className="container Right">
+      <div className="_TitleSubTitle" data-aos="fade-up">
+        <h6 className="_BgTitle Right">Actualités</h6>
+        <h3 className="_BgSubTitle Right">Actualités</h3>
+      </div>
+      <div className="container Right blogs">
       {articles.length === 0 ? (
         <p>No articles found!</p>
-      ) : (
+      ) 
+      :
+      (
         articles.map(
           ({
             id,
@@ -32,33 +40,19 @@ export default function Blogs() {
             blogImg,
             blogDescription
           }) => (
-            <div className="border mt-3 p-3 bg-light" key={id}>
-              <div className="row">
-                <div className="col-3">
-                  <Link to={`/article/${id}`}>
-                    <img
-                      src={blogImg}
-                      alt="title"
-                      style={{ height: 180, width: 180 }}
-                    />
-                  </Link>
-                </div>
-                <div className="col-9 ps-3">
-                  <div className="row">
-                    
-                   
-                  </div>
-                  <h3>{blogTitle}</h3>
-
-                  <img src={blogImg}></img>
-                  <h5>{blogDescription}</h5>
-
-                </div>
-              </div>
-            </div>
+          <div className='Card __primary blog' data-aos="fade-up" key={id}>
+            <h3>{blogTitle}</h3>
+            <img
+              src={blogImg}
+              alt="title"
+              style={{ height: 180, width: 180 }}
+            />
+            <p>{blogDescription}</p>
+          </div>
           )
         )
       )}
+    </div>
     </div>
   );
 }
