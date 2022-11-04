422e88a16273fcef1237517430f6823ac109e800
nburd01 <n.burdon@protonmail.com>
2022-11-04 12:37:32 +0100

    article layout

--- src/components/Blog/Article.jsx
+++ src/components/Blog/Article.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "../../firebase";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebase";
+import './Article.scss'
 
 export default function BlogArticle() {
   const { id } = useParams();
@@ -18,7 +19,7 @@ export default function BlogArticle() {
 
   
   return (
-    <div className="container border bg-light" style={{ marginTop: 70 }}>
+    <div className="container border bg-light" style={{ marginTop: 70,marginBottom: 70 }}>
       {article && (
         <div className="row">
           <div className="col-3">
@@ -30,20 +31,10 @@ export default function BlogArticle() {
           </div>
           <div className="col-9 mt-3">
             <h2>{article.blogTitle}</h2>
-            {/* <h5>Author: {article.createdBy}</h5> */}
-            {/* <div> Posted on: {article.createdAt.toDate().toDateString()}</div> */}
             <hr />
             <p>{article.blogBody}</p>
-            <button src={article.linkUrl}>accéder à l'article</button>
+            <a href={article.linkUrl} target="_blank" rel="noreferrer" className="blogButton">accéder à l'article</a>
 
-            {/* <div className="d-flex flex-row-reverse">
-              {user && <LikeArticle id={id} likes={article.likes} />}
-              <div className="pe-2">
-                <p>{article.likes.length}</p>
-              </div>
-            </div> */}
-            {/* comment  */}
-            {/* <Comment id={article.id} /> */}
           </div>
         </div>
       )}
