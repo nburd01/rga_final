7e90c50840659c8e9ab10a65a62bc26893ab734c
nburd01 <n.burdon@protonmail.com>
2022-11-08 18:47:14 +0100

    meta description test

--- src/pages/RGA/RGA.jsx
+++ src/pages/RGA/RGA.jsx
@@ -10,12 +10,12 @@ import { Helmet } from 'react-helmet';
 export const RGA = () => {
   return (
     <div className='RGA'>
-      <SEO
-      //   title="Bienvenue à la Roazhon Goal Academy"
-      //   description="Bienvenue à la Roazhon Goal Academy. Nous proposons des entraînements spécifiques de gardiens de but au travers de séances techniques et ludiques pour les 10 - 13 ans à proximité de Rennes !"
-      //   name="Bienvenue à la Roazhon Goal Academy"
-      //   type="accueil"
-      // />
+      {/* <SEO
+        title="Bienvenue à la Roazhon Goal Academy"
+        description="Bienvenue à la Roazhon Goal Academy. Nous proposons des entraînements spécifiques de gardiens de but au travers de séances techniques et ludiques pour les 10 - 13 ans à proximité de Rennes !"
+        name="Bienvenue à la Roazhon Goal Academy"
+        type="accueil"
+      /> */}
       <Helmet>
         <title>A propos</title>
         <meta name="title" content="Ceci est la RGA !"/>
