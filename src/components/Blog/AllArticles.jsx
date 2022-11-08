614df3dd1937ad7848d7d605717794bbcebb5e64
nburd01 <n.burdon@protonmail.com>
2022-11-08 10:56:51 +0100

    added all articles

---
+++ src/components/Blog/AllArticles.jsx
@@ -0,0 +1,64 @@
+import { collection, onSnapshot } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { db } from '../../firebase';
+import LoadingSpinner from '../Loading/Loading';
+import './AllArticles.module.scss'
+
+export const AllArticles = () => {
+
+    const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+      const articleRef = collection(db, "blogs");
+      // const q = query(articleRef, orderBy("createdAt"));
+      onSnapshot(articleRef, (snapshot) => {
+        const articles = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setArticles(articles);
+        console.log("articles",articles);
+      });
+    }, []);
+
+  return  <div className="container Right">
+  <div className="_TitleSubTitle" data-aos="fade-up">
+    <h6 className="_BgTitle Right">Actualités</h6>
+    <h3 className="_BgSubTitle Right">Actualités</h3>
+  </div>
+  <div className="container Right blogs">
+        {articles.length === 0 ? (
+            <>
+            <LoadingSpinner/>
+            {/* <p>Pas d'articles trouvés.</p> */}
+            </>
+        ) 
+        :
+        (
+            articles.map(
+            ({
+                id,
+                blogTitle, 
+                blogImg,
+                blogDescription
+            }) => (
+            <div className='Card __primary blog' data-aos="fade-up" key={id}>
+                <Link to={`/blog/${id}`}>
+                <img
+                    src={blogImg}
+                    alt="title"
+                    style={{ height: 180 }}
+                    />
+                </Link>
+                <h3>{blogTitle.substring(0, 100)}</h3>
+                <p>{blogDescription.substring(0, 150)}</p>
+                <Link to={`/blog/${id}`} className=""><span>Accéder</span></Link>
+            </div>
+            )
+            )
+        )}
+  </div>
+</div>
+
+}
--- src/components/Blog/SimpleSlider.jsx
+++ src/components/Blog/SimpleSlider.jsx
@@ -6,6 +6,7 @@ import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
 import "./slick.scss"; 
 import "./slick-theme.scss";
+import "../../index.scss";
 
 
 import 'slick-carousel/slick/slick.css'
@@ -17,9 +18,9 @@ export default function SimpleSlider() {
 
   const sliderSettings = {
     arrows: false,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    infinite: false,
+    infinite: true,
   
   };
 
@@ -39,48 +40,50 @@ export default function SimpleSlider() {
   }, []);
 
   return (
-    <div className='content'>
-      <div className='controls'>
+    <div className="container Right">
+      <div className="_TitleSubTitle" data-aos="fade-up">
+        <h6 className="_BgTitle Right">Actualités</h6>
+        <h3 className="_BgSubTitle Right">Actualités</h3>
+      </div>
+      <div className="">
       <button onClick={sliderRef?.slickPrev}>
           prev
         </button>
         <button onClick={sliderRef?.slickNext}>
           forward
         </button>
-      </div>
-      <div className="sliderContainer">
-      <Slider ref={setSliderRef} {...sliderSettings}>   
-      {articles.length === 0 ? (
-        <>
-        <LoadingSpinner/>
-        {/* <p>Pas d'articles trouvés.</p> */}
-        </>
-      ) 
-      :
-      (
-        articles.map(
-          ({
-            id,
-            blogTitle, 
-            blogImg,
-            blogDescription
-          }) => (
-          <div className='Card __primary blog' data-aos="fade-up" key={id}>
-            <Link to={`/blog/${id}`}>
-              <img
-                src={blogImg}
-                alt="title"
-                style={{ height: 180 }}
-                />
-            </Link>
-            <h3>{blogTitle.substring(0, 100)}</h3>
-            <p>{blogDescription.substring(0, 150)}</p>
-            <Link to={`/blog/${id}`} className="blogBtn"><span>Accéder</span></Link>
-          </div>
-          )
-        )
-      )}
-      </Slider>
+        <Slider ref={setSliderRef} {...sliderSettings}>   
+            {articles.length === 0 ? (
+                <>
+                <LoadingSpinner/>
+                {/* <p>Pas d'articles trouvés.</p> */}
+                </>
+            ) 
+            :
+            (
+                articles.map(
+                ({
+                    id,
+                    blogTitle, 
+                    blogImg,
+                    blogDescription
+                }) => (
+                <div className='' data-aos="fade-up" key={id}>
+                    <Link to={`/blog/${id}`}>
+                    <img
+                        src={blogImg}
+                        alt="title"
+                        style={{ height: 180 }}
+                        />
+                    </Link>
+                    <h3>{blogTitle.substring(0, 100)}</h3>
+                    <p>{blogDescription.substring(0, 150)}</p>
+                    <Link to={`/blog/${id}`} className=""><span>Accéder</span></Link>
+                </div>
+                )
+                )
+            )}
+        </Slider>
       </div>
     </div>
   )
