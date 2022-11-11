5e2ca08ff63abd58f99e989a99707d4f20cb34b6
nburd01 <n.burdon@protonmail.com>
2022-11-11 12:24:19 +0100

    added contacts render with Firebase

--- src/components/Contact/Telephone.jsx
+++ src/components/Contact/Telephone.jsx
@@ -1,28 +1,60 @@
-import React from 'react'
-import './Contact.scss'
+import { collection, onSnapshot} from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { db } from "../../firebase";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import LoadingSpinner from "../Loading/Loading";
 
 export const Telephone = () => {
-  // const emailData = useSelector((state) => state.contact.data);
-  // console.log("telephoneData",emailData.téléphone)
+  const [téléphones, setTéléphones] = useState([]);
+
+  useEffect(() => {
+    const articleRef = collection(db, "contacts");
+    onSnapshot(articleRef, (snapshot) => {
+      const téléphones = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTéléphones(téléphones);
+      console.log("email",téléphones[0].email);
+    });
+  }, []);
+
   return (
-<div className='container Right'>
-    <div className="_TitleSubTitle" data-aos="fade-up">
-      <h6 className="_BgTitle Right">Téléphone</h6>
-      <h3 className="_BgSubTitle Right">Téléphone</h3>
-    </div>
-    <div className="container Right">
-    <div className='Card __primary' data-aos="fade-up">
-    <div className="quote-wrapper">
-      <div className="contactIcon">
-          <FontAwesomeIcon icon="fa-solid fa-phone" />
+    <div className='container Right'>
+        <div className="_TitleSubTitle" data-aos="fade-up">
+          <h6 className="_BgTitle Right">Téléphone</h6>
+          <h3 className="_BgSubTitle Right">Téléphone</h3>
+        </div>
+        <div className="container Right">
+        <div className='Card __primary' data-aos="fade-up">
+        {téléphones.length === 0 ? (
+          <>
+          <LoadingSpinner/>
+          </>
+        ) 
+        :
+        (
+          téléphones.slice(0, 1).map(
+            ({
+              id,
+              téléphone, 
+            }) => (
+            <div className="quote-wrapper">
+              <div className="contactIcon">
+                <div key={id}>
+                    <div className="quote-wrapper">
+                      <FontAwesomeIcon icon="fa-solid fa-phone" />
+                      <a>{téléphone}</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            )
+          )
+        )}
+        </div>
       </div>
     </div>
-     {/* <a href="tel:{emailData.téléphone}">{emailData.téléphone}</a> */}
-     <a href="tel:0688350498">0688350498</a>
-     </div>
-    </div>
-  </div>
   )
 }
