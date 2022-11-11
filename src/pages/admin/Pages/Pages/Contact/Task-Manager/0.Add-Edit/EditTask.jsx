608f950b83213832aed6453b31cb906d7e30e34c
nburd01 <n.burdon@protonmail.com>
2022-11-11 11:29:37 +0100

    admin cleaning

--- src/pages/admin/Pages/Pages/Contact/Task-Manager/0.Add-Edit/EditTask.jsx
+++ src/pages/admin/Pages/Pages/Contact/Task-Manager/0.Add-Edit/EditTask.jsx
@@ -3,50 +3,54 @@ import {useState} from 'react'
 import './editTask.scss'
 import { doc, updateDoc } from "firebase/firestore";
 import {db} from '../../../../../../../firebase'
-import { useDispatch } from "react-redux";
-import { setReduxContact } from "../../../../../../../store/contactSlice";
 
-function EditTask({open, onClose, toEditEmail, toEditTéléphone, id}) {
 
-  const [email, setEmail] = useState(toEditEmail)
-  const [téléphone, setTéléphone] = useState(toEditTéléphone)
-  const dispatch = useDispatch();
+
+function EditTask({open, email, téléphone, onClose, toEditEmail, toEditTéléphone, id}) {
+
+  const [newEmail, setNewEmail] = useState(toEditEmail)
+  const [newTéléphone, setNewTéléphone] = useState(toEditTéléphone)
   console.log("email",email)
-
-  const [newEmail, setNewEmail] = useState("")
-  const [newTelephone, setNewTelephone] = useState("")
   
 
   /* function to update firestore */
-  const handleUpdate = async (e) => {
-    e.preventDefault()
-    dispatch(
-      setReduxContact({
-        email: newEmail,
-        téléphone: newTelephone,
-      }),
-      // setReduxTelephone({
-      // })
-    );
-    const taskDocRef = doc(db, 'contacts', id)
-    try{
-      await updateDoc(taskDocRef, {
-        email: newEmail,
-        téléphone: newTelephone,
-      })
-      onClose()
-    } catch (err) {
-      alert(err)
-    } 
-  }
+    const handleUpdateEmail = async (e) => {
+      e.preventDefault()
+      const taskDocRef = doc(db, 'blogs', id)
+      try{
+        await updateDoc(taskDocRef, {
+          email: newEmail, 
+        })
+        onClose()
+      } catch (err) {
+        alert(err)
+      }
+    }
+
+    const handleUpdateTelephone = async (e) => {
+      e.preventDefault()
+      const taskDocRef = doc(db, 'blogs', id)
+      try{
+        await updateDoc(taskDocRef, {
+          téléphone: newTéléphone,   
+        })
+        onClose()
+      } catch (err) {
+        alert(err)
+      }
+    }
 
   return (
     <Modal modalLable='Modifier' onClose={onClose} open={open}>
-      <form onSubmit={handleUpdate} className='editTask'>
-        <input type='text' name='email' onChange={(e) => setNewEmail(e.target.value)} value={newEmail} />
-        <input type='text' name='téléphone' onChange={(e) => setNewTelephone(e.target.value)} value={newTelephone}/>
-        <button type='submit'>Soumettre</button>
-      </form>
+      <>
+        <p>Email</p>
+        <input type='text' name='Email' onChange={(e) => setNewEmail(e.target.value)} placeholder='Email' defaultValue={email} maxlength="100"/>
+        <button onClick={handleUpdateEmail}>Mettre à jour l'email</button>
+        
+        <p>Téléphone</p>
+        <input type='text' name='Téléphone' onChange={(e) => setNewTéléphone(e.target.value)} placeholder="Téléphone" defaultValue={téléphone}/>
+        <button onClick={handleUpdateTelephone}>Mettre à jour le numéro de téléphone</button>
+      </>
     </Modal>
   )
 }
