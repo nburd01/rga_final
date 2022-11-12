0e4d9159047ca02235c13b1830e7d245bdf67716
nburd01 <n.burdon@protonmail.com>
2022-11-12 15:49:28 +0100

    added routes

--- src/pages/admin/Pages/AdmPages.jsx
+++ src/pages/admin/Pages/AdmPages.jsx
@@ -28,6 +28,9 @@ export const AdmPages = () => {
           <li>
             <Link to="/admin/contact"> Contact </Link>
           </li>
+          <li>
+            <Link to="/admin/blog"> Blog </Link>
+          </li>
         </ul>
       </div>
       <div className='content'>
