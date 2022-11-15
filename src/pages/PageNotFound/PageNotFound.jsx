c509dcfc71d5b2db5d6d138204eaa263d54fbb8e
nburd01 <n.burdon@protonmail.com>
2022-11-15 11:41:34 +0100

    404

---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './PageNotFound.scss'
+
+export default function PageNotFound() {
+  return (
+    <div className='container NotFound'>
+      <h1>404</h1>
+      <p>Nous ne trouvons pas la page souhaitée</p>
+    </div>
+  )
+}
